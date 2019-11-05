1a45118c4a2aa3e3a3f9ad1ac7b13b2d12788bc5
ArnaudDuthoit <43751449+ArnaudDuthoit@users.noreply.github.com>
2019-11-05 14:19:31 +0100

    Firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = firebase.initializeApp({
+    apiKey: '<your-api-key>',
+    authDomain: '<your-auth-domain>',
+    databaseURL: '<your-database-url>',
+    projectId: '<your-cloud-firestore-project>',
+    storageBucket: '<your-storage-bucket>',
+    messagingSenderId: '<your-sender-id>'
+});
+
+export {firebaseConfig as firebase};

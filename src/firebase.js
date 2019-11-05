b8d46668b0279819817a698ee6e2cb63ed66839c
ArnaudDuthoit <43751449+ArnaudDuthoit@users.noreply.github.com>
2019-11-05 16:54:53 +0100

    Tasks

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,15 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
 const firebaseConfig = firebase.initializeApp({
-    apiKey: '<your-api-key>',
-    authDomain: '<your-auth-domain>',
-    databaseURL: '<your-database-url>',
-    projectId: '<your-cloud-firestore-project>',
-    storageBucket: '<your-storage-bucket>',
-    messagingSenderId: '<your-sender-id>'
+    apiKey: "AIzaSyDAmEnX-xqbKhRv6HdEIRAhFCf9uUmcpkQ",
+    authDomain: "react-todolist-51d51.firebaseapp.com",
+    databaseURL: "https://react-todolist-51d51.firebaseio.com",
+    projectId: "react-todolist-51d51",
+    storageBucket: "react-todolist-51d51.appspot.com",
+    messagingSenderId: "697009033884",
+    appId: "1:697009033884:web:e6b7780041b166a1f05359"
+
 });
 
 export {firebaseConfig as firebase};
